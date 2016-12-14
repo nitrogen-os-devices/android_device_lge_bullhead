6ff6aad163c329fe03e106dc84970427cb19b7e2
xyyx <xyyx@mail.ru>
2016-12-14 08:49:09 +0800

    Use height and width for bootanimation

--- nitrogen_bullhead.mk
+++ nitrogen_bullhead.mk
@@ -14,6 +14,10 @@
 # limitations under the License.
 #
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
