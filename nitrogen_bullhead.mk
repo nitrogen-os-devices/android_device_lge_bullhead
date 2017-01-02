09180fc1fab9258e8bff90de10aedd55653ba226
Valeriy <mexanicteam@gmail.com>
2017-01-02 16:03:32 +0400

    Kill google fingerprint/build fingerprint

--- nitrogen_bullhead.mk
+++ nitrogen_bullhead.mk
@@ -37,12 +37,3 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5X
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
-
-USE_AOSP_FINGERPRINT := false
-
-ifeq ($(USE_AOSP_FINGERPRINT),true)
-    PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRODUCT_NAME=bullhead \
-        BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.1/NMF26F/3425388:user/release-keys" \
-        PRIVATE_BUILD_DESC="bullhead-user 7.1.1 NMF26F 3425388 release-keys"
-endif
